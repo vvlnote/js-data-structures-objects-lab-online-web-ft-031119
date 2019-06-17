02530c0ca135d66fa82032166cfa5b9a896a10a5
Joy Lin <vvlnote@hotmail.com>
2019-06-17 00:20:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   const newDriver = {};
   Object.assign(newDriver, driver);
+  console.log(`before deletion : newDriver = ${newDriver}, driver = ${driver}`);
   delete newDriver.key;
+    console.log(`after deletion : newDriver = ${newDriver}, driver = ${driver}`);
   return newDriver;
 }
