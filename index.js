47a8d2ef679d64f4ab6c698fc0f1eaa8e57741f9
Joy Lin <vvlnote@hotmail.com>
2019-06-17 00:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  console.log(driver, key, value);
   let newDriver = {};
   Object.assign(newDriver, driver, {[key]:value});
-  console.log(newDriver);
   return newDriver;
 }
